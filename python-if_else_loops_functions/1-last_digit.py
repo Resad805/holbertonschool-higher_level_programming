325a96a1e640e1b13e708e99f75f4c9dc6d76bfa
Resad805 <aabbasov1234567@gmail.com>
2025-11-24 12:14:01 +0000

    commit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -12,6 +12,6 @@ last_print = print(f"Last digit of {number} is {ls} and is",end=" ")
 if ls > 5:
 	print("greater than 5")
 elif ls == 0:
-        print("and is 0")
+        print(" 0") 
 elif ls < 6 and not 0:
         print("less than 6 and not 0")
