6cdd1e70dd771ec4d7315f3679be12b348c74a2f
Resad805 <aabbasov1234567@gmail.com>
2025-11-28 14:41:53 +0000

    s

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number>0:
+    if number > 0:
         lastdigit = number % 10
-    elif number<0:
+    elif number < 0:
         lastdigit = (number * -1) %10
     elif number == 0:
         lastdigit = 0
