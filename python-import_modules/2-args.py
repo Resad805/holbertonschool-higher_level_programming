b2cf8316996dc6c3bcefa962bb2fa2a9d216f01e
Resad805 <aabbasov1234567@gmail.com>
2025-12-03 16:14:31 +0000

    x

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
         print(f'1: {sys.argv[1]}')
     elif args > 1:
         print(f'{args} arguments:')
-        for i in range(1, args +1):
+        for i in range(1, args +1 ):
             print(f'{i}: {sys.argv[i] }')
