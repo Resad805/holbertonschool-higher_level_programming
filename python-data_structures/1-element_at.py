3be4806552aaf19b981ef51bff8f045c72268fdc
Resad805 <aabbasov1234567@gmail.com>
2025-12-04 14:37:20 +0000

    cx

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for i in indx:
-        if i < 0:
-            return None
-        elif len(my_list) < len(idx):
-            return None
+    if idx < 0:
+        return None
+    elif len(my_list) < len(idx):
+        return None
     return my_list[idx]
