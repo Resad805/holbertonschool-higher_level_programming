c7353ce48c65339cfc89e317f9c1286a2fd4ce6a
Resad805 <aabbasov1234567@gmail.com>
2025-12-05 13:40:12 +0000

    tuple

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,6 +4,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     y = tuple_y[0] if len(tuple_y) > 0 else 0
 
     x1 = tuple_x[1] if len(tuple_x) > 1 else 0
-    y1 = tuple_by[1] if len(tuple_y) > 1 else 0
+    y1 = tuple_y[1] if len(tuple_y) > 1 else 0
 
     return (x+y, x1+y1)
