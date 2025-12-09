7919289cecaa71f448fc147fc7f54e31ef7c4771
Resad805 <aabbasov1234567@gmail.com>
2025-12-09 11:46:30 +0000

    x

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -4,7 +4,7 @@ my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
 
 def search_replace(my_list, search, replace):
     new_list= []
-    new_list.copy(my_list)
+    new_list = my_list.copy()
     for i in new_list:
         if i == search:
             new_list[i] = replace
