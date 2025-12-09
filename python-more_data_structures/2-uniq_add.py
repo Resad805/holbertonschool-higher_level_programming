b395e5c7fae001de57212a01952c82c665666014
Resad805 <aabbasov1234567@gmail.com>
2025-12-09 12:30:29 +0000

    x

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -6,6 +6,6 @@ def uniq_add(my_list=[]):
     result = 0
 
     for i in my_list:
-        if i in not new_list:
+        if i not in new_list:
             result += i
     return print(f'Result: {result}')
