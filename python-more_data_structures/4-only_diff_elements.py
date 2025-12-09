c49399f82e71824a5999bf1f6ad5209600f031cf
Resad805 <aabbasov1234567@gmail.com>
2025-12-09 13:20:04 +0000

    x

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -2,11 +2,11 @@
 
 
 def only_diff_elements(set_1, set_2):
-    z = set_1.copy()
+    od_set = set_1.copy()
     new = []
-    for i in z:
+    for i in od_set:
         if i in set_2:
             new.append(i)
     for i in new:
-        z.remove(i)
-    return print(sorted(list(z)))
+        od_set.remove(i)
+    return print(sorted(list(od_set)))
