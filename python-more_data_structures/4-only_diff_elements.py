2a6d91c0bb1e3ea316b4d38865494ef6f58a20c9
Resad805 <aabbasov1234567@gmail.com>
2025-12-09 13:16:05 +0000

    x

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def common_elements(set_1, set_2):
+    z = set_1.copy()
+    new = []
+    for i in z:
+        if i in set_2:
+            new.append(i)
+    for i in new:
+        z.remove(i)
+    return print(sorted(list(z)))
