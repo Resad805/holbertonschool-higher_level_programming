fa2ef68a6e8864199d2b5f4bd414d650e2c4fb2c
Resad805 <aabbasov1234567@gmail.com>
2025-12-09 12:29:44 +0000

    x

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -3,8 +3,9 @@ my_list = [1, 2, 3, 1, 4, 2, 5]
 
 
 def uniq_add(my_list=[]):
-    for i in range(len(my_list)):
-        for j in range(i , len(my_list)):
-            if i == j:
-                my_list.remove(j)
-    return my_list
+    result = 0
+
+    for i in my_list:
+        if i in not new_list:
+            result += i
+    return print(f'Result: {result}')
