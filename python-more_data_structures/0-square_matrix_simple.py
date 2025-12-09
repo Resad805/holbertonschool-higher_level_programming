60d56b286e8758e3186870d6efc0bacdceb24853
Resad805 <aabbasov1234567@gmail.com>
2025-12-09 11:11:11 +0000

    x

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]]
     
 def square_matrix_simple(matrix=[]):
     nmatrix = []
     nrow = []
     for i in matrix:
         for j in i:
-            nrow.append(i*i)
+            squared_num = i*i
+            nrow.append(squared_num)
         nmatrix.append(nrow)
     return nmatrix
