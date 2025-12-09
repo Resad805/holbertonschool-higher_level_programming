201085d7750ecbb49b002beb19e7873c002d4b4e
Resad805 <aabbasov1234567@gmail.com>
2025-12-09 11:29:38 +0000

    x

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+
+def search_replace(my_list, search, replace):
+    new_list= []
+    new_list.append(mylist)
+    x = new_list.replace(2,89)
+    return new_list
