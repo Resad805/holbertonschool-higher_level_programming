50716c6005afd91b99277635b0c32f67b5fcb9a7
Resad805 <aabbasov1234567@gmail.com>
2025-12-09 13:24:30 +0000

    x

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -6,7 +6,7 @@ def only_diff_elements(set_1, set_2):
     for i in set_1:
         if i not in set_1:
             final.append(i)
-    for i in set2:
+    for i in set_2:
         if i not in set_2:
             final.append(i)
 
