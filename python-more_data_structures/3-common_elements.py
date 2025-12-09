0a4e0ae2a93d96599922f3d12ad17ab221fa344a
Resad805 <aabbasov1234567@gmail.com>
2025-12-09 12:54:36 +0000

    s

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+
+
+def common_elements(set_1, set_2):
+    z = set_1.copy()
+    new = {}
+    for i in z:
+        if i not in set_2:
+            z.pop(i)
+    return print(list(z))
