e7f5f9d96709394e416b11cf90566ed3bc5b6ec2
Resad805 <aabbasov1234567@gmail.com>
2025-12-11 10:08:52 +0000

    x

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
-
-
 def print_sorted_dictionary(a_dictionary):
     keys_list = sorted(a_dictionary.keys())
 
     for keys in keys_list:
         value = a_dictionary[keys]
         print(f"{keys}: {value}")
-
