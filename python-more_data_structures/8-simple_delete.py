8f6312a46b2655b7ec1e52dc75fde19ed43e7be2
Resad805 <aabbasov1234567@gmail.com>
2025-12-11 10:45:25 +0000

    s

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
+    for i in a_dictionary:
+        if key == i:
+            continue
     a_dictionary.pop(key)
     return a_dictionary
