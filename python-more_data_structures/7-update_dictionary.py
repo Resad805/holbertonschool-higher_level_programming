955c709caaa7f07abef3cd6db07b9b8931a2eff8
Resad805 <aabbasov1234567@gmail.com>
2025-12-11 10:29:35 +0000

    s

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key:str, value):
+    sorted_keys = sorted(a_dictionary.keys())
+    sorted_dict = sorted(a_dictionary)
+    for i in sorted_dict:
+        if sorted_keys == key:
+            a_dictionary.update({i}:{value})
+        else:
+            a_dictionary[key] = value
+        print(f"{keys}: {value}")
