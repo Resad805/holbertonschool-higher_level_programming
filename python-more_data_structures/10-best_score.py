14ae78f999c123c22efa3d3602045b5db1dd25e0
Resad805 <aabbasov1234567@gmail.com>
2025-12-12 23:50:51 +0000

    s

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,10 +3,5 @@
 def best_score(a_dictionary):
     ndict = a_dictionary.copy()
     dicts = a_dictionary.values()
-    for i in dicts:
-        for j in dicts[i+1]:
-            if i > j:
-                ndict.pop(j)
-            else:
-                ndict.pop(i)
-    return ndict
+    
+    print(f"Best score: {dicts[-1]}")
