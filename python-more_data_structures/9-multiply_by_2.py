c28575182c6b1a58bc0c6b4a4ea3c716f97aef36
Resad805 <aabbasov1234567@gmail.com>
2025-12-12 23:21:53 +0000

    x

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -3,4 +3,4 @@ def multiply_by_2(a_dictionary):
     new_dict = a_dictionary.copy()
     key = a_dictionary.keys()
     multiply = a_dictionary[key] * 2 
-    return print(f"{key: multiply}")
+    print(f"{key: multiply}")
