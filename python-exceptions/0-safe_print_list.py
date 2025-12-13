17b97fe62922bde019abff5abc069bad817d3805
Resad805 <aabbasov1234567@gmail.com>
2025-12-13 00:33:28 +0000

    s

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    printed_count = 0
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            printed_count += 1
+
+    except IndexError:
+        pass
+    return printed_count
