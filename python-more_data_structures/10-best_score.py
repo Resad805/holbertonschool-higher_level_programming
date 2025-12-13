4d7580591acdddcd7d72e6918bb5208b570e294f
Resad805 <aabbasov1234567@gmail.com>
2025-12-13 00:11:26 +0000

    s

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     current_best_key = None
-    current_best_score = -1 
+    current_best_score = -1
     for key, value in a_dictionary.items():
         if value > current_best_score:
             current_best_score = value
