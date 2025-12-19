9c5f84e82d1926dcf00cb4bffd0f6351bf9b054d
Resad805 <aabbasov1234567@gmail.com>
2025-12-19 11:14:38 +0000

    s

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -12,4 +12,5 @@ class Square:
             raise ValueError("size must be >= 0")
 
     def area(self):
-        return self.__size ** 2
+        self.__size = self.__size** 2
+        return self.__size
