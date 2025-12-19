e6badcfb4850cf60352472e01fcbe5b1f461fac6
Resad805 <aabbasov1234567@gmail.com>
2025-12-19 11:31:36 +0000

    s

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,12 +5,20 @@
 class Square:
     """s"""
     def __init__(self, size=0):
-        self.__size = size
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError("size must be >= 0")
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+            """Setter: sets the private __size with validation"""
+            if not isinstance(value, int):
+                raise TypeError('size must be an integer')
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.size == value
 
     def area(self):
-        x = self.__size ** 2
-        return x
+        return self.__size ** 2
