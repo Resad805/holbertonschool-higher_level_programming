88fbc7c76c8c7738cd96b2f4d2baf93225da6c5e
Resad805 <aabbasov1234567@gmail.com>
2025-12-19 13:01:13 +0000

    s

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,63 +1,54 @@
 #!/usr/bin/python3
-"""salam"""
+"""Square"""
 
 
 class Square:
-    """s"""
-    def __init__(self, size=0):
+    """R"""
+
+    def __init__(self, size=0, position=(0, 0)):
+
+        Args:
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
         self.size = size
+        self.position = position
 
     @property
     def size(self):
-        return self.__size
+        return (self.__size)
 
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
-            raise TypeError('size must be an integer')
-        if value < 0:
+            raise TypeError("size must be an integer")
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    def area(self):
-        return self.__size ** 2
-
-    def my_print(self):
-        if self.size == 0:
-            print("")
-        for i in range(self.__size):
-            print('#' * self.__size)
-
     @property
     def position(self):
-        """Position değerini döndürür"""
-        return self.__position
+        return (self.__position)
 
     @position.setter
     def position(self, value):
-        """Position değerini doğrular: 2 pozitif tam sayıdan oluşan bir tuple mı?"""
-        if (not isinstance(value, tuple) or len(value) != 2 or
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
-        """Alanı döndürür"""
-        return self.__size ** 2
+        return (self.__size * self.__size)
 
     def my_print(self):
-        """Kareyi ekrana basar. Position[1] kadar dikey, 
-        position[0] kadar yatay boşluk bırakır."""
+
         if self.__size == 0:
             print("")
             return
 
-        # Üstten dikey boşluk (Y ekseni)
-        for _ in range(self.__position[1]):
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
             print("")
-
-        # Karenin her bir satırı
-        for _ in range(self.__size):
-            # Soldan yatay boşluk (X ekseni) + Kare karakterleri
-            print(" " * self.__position[0] + "#" * self.__size)
