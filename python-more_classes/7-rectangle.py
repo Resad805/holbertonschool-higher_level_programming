5f2c2060299dd377d2e763623ed0ef52363af507
Resad805 <aabbasov1234567@gmail.com>
2025-12-19 13:29:04 +0000

    x

---
+++ python-more_classes/7-rectangle.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""x"""
+
+
+class Rectangle:
+    """x"""
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__height + self.__width) * 2
+
+    def __draw(self):
+        rectangle = ""
+        height = self.__height
+        width = self.__width
+        character = self.print_symbol
+
+        for i in range(height):
+            rectangle += str(character) * width
+
+            if i != height - 1:
+                rectangle += "\n"
+
+        return rectangle
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return self.__draw()
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        print("Bye rectangle...")
