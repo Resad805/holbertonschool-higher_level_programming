25862aaf9b92a4ff5d92aa90a6a5352b4e059bca
Resad805 <aabbasov1234567@gmail.com>
2025-12-26 15:16:43 +0000

    s

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
 """inheritance"""
 
+
 class BaseGeometry:
+    """s"""
+
     def area(self):
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
+
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
+    """s"""
+
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
-# Print the user-defined methods and attributes of Rectangle
-user_defined_attrs = [attr for attr in dir(Rectangle) if not attr.startswith('__')]
-print(user_defined_attrs)
+
