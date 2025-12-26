ac249703051b5b5912bfab33920c10b978719f39
Resad805 <aabbasov1234567@gmail.com>
2025-12-26 15:06:40 +0000

    s

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
-"""inheritance"""
-
-
+"""
+Bu modul həndəsi fiqurların təməlini təşkil edən 
+BaseGeometry və ondan törəyən Rectangle siniflərini saxlayır.
+"""
 class BaseGeometry:
-    """s"""
+    """
+    Həndəsi fiqurlar üçün baza sinfi.
+    """
 
     def area(self):
+        """
+        Sahəni hesablayan metod.
+        İstisnasız olaraq Exception qaytarır.
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """
+        Dəyərin tam ədəd və müsbət olduğunu yoxlayır.
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
 
@@ -16,10 +26,14 @@ class BaseGeometry:
             raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
-    """s"""
+    """
+    BaseGeometry-dən miras alan Düzbucaqlı sinfi.
+    """
     def __init__(self, width, height):
+        """
+        Yeni bir Rectangle yaradarkən en və hündürlüyü yoxlayır.
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-
         self.__width = width
         self.__height = height
