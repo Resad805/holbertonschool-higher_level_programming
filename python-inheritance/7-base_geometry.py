005c2f86e2308d11145c1306e0c23dc02b3789cd
Resad805 <aabbasov1234567@gmail.com>
2025-12-26 14:46:46 +0000

    s

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""inheritance"""
+"""BaseGeometry sinfi üçün modul."""
 
 
 class BaseGeometry:
-    """s"""
+    """Həndəsi fiqurlar üçün baza sinfi."""
 
     def area(self):
+        """Sahəni hesablamalıdır, hələ tətbiq edilməyib."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """Dəyəri tam ədəd və müsbət olması baxımından yoxlayır."""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
-
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-
