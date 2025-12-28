e774aa2b8b790222d81fe95236b8ec727c10cf24
Resad805 <aabbasov1234567@gmail.com>
2025-12-28 15:20:19 +0000

    s

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""inheritance"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """s"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size**2
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size)
