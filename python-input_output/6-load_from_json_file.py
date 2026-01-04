7672c41e4e478230be9678fb35973a6127f27867
Resad805 <aabbasov1234567@gmail.com>
2026-01-04 15:22:36 +0000

    sa

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""sa"""
-
-
+"""sa."""
 import json
 
 
-def save_to_json_file(my_obj, filename):
-    """Writes an object to a text file using JSON representation."""
-    with open(filename, "w", encoding="utf-8") as f:
-        json.dump(my_obj, f)
+def load_from_json_file(filename):
+    """Creates an Object from a “JSON file”."""
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
